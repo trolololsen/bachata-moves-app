a2d7b32d50996a33c9c22dc2fa6d464b3510561f
trolololsen <trolololsen@gmail.com>
2026-02-13 22:59:40 +0100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,9 @@
 const SUPABASE_URL = "https://asztjrzxjpdyijwlgtho.supabase.co";
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFzenRqcnp4anBkeWlqd2xndGhvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzA5ODYwNjUsImV4cCI6MjA4NjU2MjA2NX0.4KGvu4SNHXjsP2Lj5jE0du1Ky-t_W6GIY_e9HEyRYcY";
+const SUPABASE_ANON_KEY = "sb_publishable_IgFVpPYLIcjp6zFrzfUWqQ_coNYXWJb";
 
 const supabaseClient = supabase.createClient(
   SUPABASE_URL,
   SUPABASE_ANON_KEY
 );
+
 
