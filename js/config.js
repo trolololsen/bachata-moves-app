adcc4c8127d7b9d295de5b3597356e0819d89823
trolololsen <trolololsen@gmail.com>
2026-02-13 23:03:07 +0100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,10 +1,11 @@
 const SUPABASE_URL = "https://asztjrzxjpdyijwlgtho.supabase.co";
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFzenRqcnp4anBkeWlqd2xndGhvIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc3MDk4NjA2NSwiZXhwIjoyMDg2NTYyMDY1fQ.TPFLjmFDxXG34TtYOVK1mqqUbxi7s6sMbHoixs9ZIpo";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFzenRqcnp4anBkeWlqd2xndGhvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzA5ODYwNjUsImV4cCI6MjA4NjU2MjA2NX0.4KGvu4SNHXjsP2Lj5jE0du1Ky-t_W6GIY_e9HEyRYcY";
 
 const supabaseClient = supabase.createClient(
   SUPABASE_URL,
   SUPABASE_ANON_KEY
 );
+
 
 
 
